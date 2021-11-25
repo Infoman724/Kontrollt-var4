4d9f460ef60344351937d58d19e23964917c3d7a
vlademir guzov <infoman724@gmail.com>
2021-11-25 20:19:10 +0200

    Final

--- Kontrolltöö ül1/Kontrolltöö_ül1.py
+++ Kontrolltöö ül1/Kontrolltöö_ül1.py
@@ -1,8 +1,14 @@
 #1ое задание
 n=int(input("Сколько зверьков рисуем от одного до девяти?" ))
-osa1=print(" ^---^")
-osa2=print("( o o )")
-osa3=print("! = !/)")
+cat=[' ^---^',
+      '( o o )',
+      ' ! = !/)']
+
+
+for i in cat:
+    print((i+" ") * n)
+
+
 
 print()
 print()
@@ -18,5 +24,47 @@ while n != x:
             print(x)
         elif n <= x:
             break
+
 print()
 print()
+print()
+#3ье задание
+from random import*
+max=1
+min=5
+k=randint(2,31)
+print("В классе учеников:")
+print(k)
+print("Оценки учеников:")
+for i in range(1,k):
+    i=randint(1,5)
+    if i>max:
+        max=i
+    if i<min:
+        min=i
+    print(i)
+print("Максимальный результат",max)
+
+print("Минимальный результат",min)
+
+
+
+print()
+print()
+print()
+
+#4ое задание
+for i in range(1, 9):
+    print("микробов",2 ** i)
+
+print()
+print()
+#5ое задание
+k=int(input("Сколько котлет у Губки боба? "))
+
+m=int(input("Сколько помещается на сковородку? "))
+
+fullpan=k//m
+print("Пожариться полных сковородок:",fullpan)
+another=k%m
+print("Останеться пожврить:",another)
